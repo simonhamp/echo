3c14ea7fdd9e4d36ccf6d5b6ea926e101272b2de
Simon Hamp <simon@flipstorm.co.uk>
2018-02-18 16:35:53 +0000

    Add Socket ID generation

--- src/connector/ratchet-connector.ts
+++ src/connector/ratchet-connector.ts
@@ -41,6 +41,8 @@ export class RatchetConnector extends Connector {
         // Extend the socket with a queue for events
         this.socket.queue = [];
 
+        this.socket.id = this.generateId();
+
         // Extend the socket with an emit function (mimic SocketIO API)
         this.socket.emit = (event: string, message: object) => {
             return this.emit(event, message);
@@ -206,6 +208,55 @@ export class RatchetConnector extends Connector {
      */
     socketId(): string {
         return this.socket.id;
+    }
+
+    /**
+     * Generate an ID for the socket.
+     *
+     * @see https://jsperf.com/uuid4/8
+     * @return {string}
+     */
+    generateId(): string {
+        var c = '0123456789ABCDEF'.split(''),
+            id = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.split(''),
+            r;
+
+        id[0] = c[(r = Math.random() * 0x100000000) & 0xf];
+        id[1] = c[(r >>>= 4) & 0xf];
+        id[2] = c[(r >>>= 4) & 0xf];
+        id[3] = c[(r >>>= 4) & 0xf];
+        id[4] = c[(r >>>= 4) & 0xf];
+        id[5] = c[(r >>>= 4) & 0xf];
+        id[6] = c[(r >>>= 4) & 0xf];
+        id[7] = c[(r >>>= 4) & 0xf];
+
+        id[9] = c[(r = Math.random() * 0x100000000) & 0xf];
+        id[10] = c[(r >>>= 4) & 0xf];
+        id[11] = c[(r >>>= 4) & 0xf];
+        id[12] = c[(r >>>= 4) & 0xf];
+        id[15] = c[(r >>>= 4) & 0xf];
+        id[16] = c[(r >>>= 4) & 0xf];
+        id[17] = c[(r >>>= 4) & 0xf];
+
+        id[19] = c[(r = Math.random() * 0x100000000) & 0x3 | 0x8];
+        id[20] = c[(r >>>= 4) & 0xf];
+        id[21] = c[(r >>>= 4) & 0xf];
+        id[22] = c[(r >>>= 4) & 0xf];
+        id[24] = c[(r >>>= 4) & 0xf];
+        id[25] = c[(r >>>= 4) & 0xf];
+        id[26] = c[(r >>>= 4) & 0xf];
+        id[27] = c[(r >>>= 4) & 0xf];
+
+        id[28] = c[(r = Math.random() * 0x100000000) & 0xf];
+        id[29] = c[(r >>>= 4) & 0xf];
+        id[30] = c[(r >>>= 4) & 0xf];
+        id[31] = c[(r >>>= 4) & 0xf];
+        id[32] = c[(r >>>= 4) & 0xf];
+        id[33] = c[(r >>>= 4) & 0xf];
+        id[34] = c[(r >>>= 4) & 0xf];
+        id[35] = c[(r >>>= 4) & 0xf];
+
+        return id.join('');
     }
 
     /**
